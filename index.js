1607d6ce9605a6f36337302d80f247456e2cdf78
Andre Natal <anatal@gmail.com>
2016-06-30 00:53:25 -0300

    Communication with the server working. Webrtc Vad integrated in the code, but the cut still happens by timeout Orchestration complete

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 var Wakeword = require('wakeword');
 var MemoryStream = require('memorystream');
+var WebSocket = require('ws');
 
-const MAX_LISTEN_TIME = 5000;
+const MAX_LISTEN_TIME = 7500;
 const VAD_BYTES = 640;
+var ws;
+var streamServer = new MemoryStream();
+
+function connectServer(){
+  console.log('connecting to server');
+  ws = new WebSocket('wss://localhost:8080/?token=testtoken', null, { rejectUnauthorized: false });
+
+
+  ws.on('open', () => {
+    console.log("socket opened");
+  });
+
+
+  ws.on('message', (data, flags) => {
+    console.log('message from server rcvd:' );
+  });
+
+  ws.on('close', () => {
+    console.log('message closed from srv');
+  });
+}
 
 function streamToServer(data) {
-  // TODO: Send data to server
+  console.log('Sending data to server');
+  streamServer.write(data);
+  var samples;
+  if (ws.readyState == ws.OPEN) {
+    console.log("entrando no while de ler samples " );
+
+    while ((samples = streamServer.read(VAD_BYTES))) {
+      console.log("sending to server...");
+      ws.send(samples);
+    }
+  } else {
+    console.log("not connected yet");
+  }
 }
 
 function endStreamToServer() {
-  // TODO: Close stream (and listen for result?)
   console.log('Ending stream to server');
+  if (ws.readyState == ws.OPEN) {
+    ws.send('EOS');
+  } else {
+    console.log("not connected yet");
+  }
 }
 
 function vad(data) {
@@ -25,24 +63,32 @@ function vad(data) {
 }
 
 function listen() {
-  var stream = null;
+  var streamvad = null;
   var wakeTime = 0;
-  Wakeword.listen(['foxy'], 0.80, (data, word) => {
-    if (!stream) {
+
+  Wakeword.listen(['foxy'], 0.83, (data, word) => {
+
+    if (!streamvad) {
+      connectServer();
+      streamvad = new MemoryStream();
       wakeTime = Date.now();
-      stream = new MemoryStream();
     }
 
-    stream.write(data);
+    streamvad.write(data);
 
-    var samples;
-    while ((samples = stream.read(VAD_BYTES))) {
+    console.log('foxy');
+
+    while ((samples = streamvad.read(VAD_BYTES))) {
+      vad(samples);
       streamToServer(samples);
-      if (Date.now() - wakeTime > MAX_LISTEN_TIME || !vad(samples)) {
-        Wakeword.stop();
-        stream.end();
-        endStreamToServer();
-      }
+    }
+
+    if (Date.now() - wakeTime > MAX_LISTEN_TIME) {
+      streamvad.end();
+      Wakeword.stop();
+      endStreamToServer();
+      listen();
+      console.log("Parado e reiniciado")
     }
   });
 }
