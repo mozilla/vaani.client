e39d43b7f2b05abfecff094c29a96742d1a7d60a
Andre Natal <anatal@gmail.com>
2016-07-01 17:48:15 -0300

    Whole cycle working still cutting by timeout

--- index.js
+++ index.js
@@ -1,44 +1,63 @@
+"use strict";
+
 var Wakeword = require('wakeword');
 var MemoryStream = require('memorystream');
 var WebSocket = require('ws');
-
+const child_process = require('child_process');
+const fs = require('fs');
 const MAX_LISTEN_TIME = 7500;
 const VAD_BYTES = 640;
 var ws;
 var streamServer = new MemoryStream();
+const call = (command, params) => child_process.spawn(command, params.split(' '));
 
 function connectServer(){
   console.log('connecting to server');
-  ws = new WebSocket('wss://localhost:8080/?token=testtoken', null, { rejectUnauthorized: false });
-
+  ws = new WebSocket('ws://localhost:8080/?token=testtoken', null, { rejectUnauthorized: false });
+  var wstream;
+  var isWav;
 
   ws.on('open', () => {
     console.log("socket opened");
-  });
+    isWav = false;
+    wstream = fs.createWriteStream('output.wav');
+});
 
 
   ws.on('message', (data, flags) => {
-    console.log('message from server rcvd:' );
+    //console.log('message from server rcvd:');
+    if (!isWav){
+      console.log(data);
+      isWav = true;
+    } else {
+      wstream.write(data);
+    }
   });
 
   ws.on('close', () => {
     console.log('message closed from srv');
+    wstream.end();
+    var play = call('play', 'output.wav');
+    play.stdout.on('close', () => {
+      console.log('fim do play. Restart.....');
+      listen();
+    });
   });
 }
 
 function streamToServer(data) {
-  console.log('Sending data to server');
+  //console.log('Sending data to server');
   streamServer.write(data);
   var samples;
   if (ws.readyState == ws.OPEN) {
-    console.log("entrando no while de ler samples " );
+    //console.log("entrando no while de ler samples " );
 
     while ((samples = streamServer.read(VAD_BYTES))) {
-      console.log("sending to server...");
+      //console.log("sending to server...");
       ws.send(samples);
     }
   } else {
-    console.log("not connected yet");
+    //console.log("not connected yet");
   }
 }
 
@@ -47,7 +66,7 @@ function endStreamToServer() {
   if (ws.readyState == ws.OPEN) {
     ws.send('EOS');
   } else {
-    console.log("not connected yet");
+    //console.log("not connected yet");
   }
 }
 
@@ -57,7 +76,7 @@ function vad(data) {
   }
 
   // TODO: return false if no activity detected.
-  console.log('data.... Vad:' +  Wakeword.decoder.processWebrtcVad(data));
+  //console.log('data.... Vad:' +  Wakeword.decoder.processWebrtcVad(data));
 
   return true;
 }
@@ -76,8 +95,8 @@ function listen() {
 
     streamvad.write(data);
 
-    console.log('foxy');
-
+    //console.log('foxy');
+    let samples;
     while ((samples = streamvad.read(VAD_BYTES))) {
       vad(samples);
       streamToServer(samples);
@@ -87,8 +106,7 @@ function listen() {
       streamvad.end();
       Wakeword.stop();
       endStreamToServer();
-      listen();
-      console.log("Parado e reiniciado")
+      console.log("Parado ws.")
     }
   });
 }
