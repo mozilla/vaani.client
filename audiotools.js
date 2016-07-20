060f33f36ae2c0025ac51b8adcc8e7669d092373
Andre Natal <anatal@gmail.com>
2016-07-20 15:16:48 -0300

    Setting soundboard levels at 100% at the boot

--- audiotools.js
+++ audiotools.js
@@ -29,6 +29,12 @@ module.exports =  {
     shelloutSync: (command, params) => child_process.spawnSync(command, params.split(' ')),
 
     setup: function(wakeword, config){
+
+        if (process.platform === "linux") {
+            this.shelloutSync('amixer',  "-c 2 set PCM 100%");
+            this.shelloutSync('amixer',  "-c 3 set PCM 100%");
+        }
+
         this.config = config;
         this.wakeword = wakeword;
         this.wakeword.deviceName = this.config.micdevicename;
@@ -36,6 +42,9 @@ module.exports =  {
         this.microphone.pause();
         this.playaudio('resources/start.wav');
         this.microphone.resume();
+
+
+
     },
 
     playaudio: function(path){
